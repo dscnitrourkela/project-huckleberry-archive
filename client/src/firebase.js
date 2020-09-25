da6024773dfb870dd69551c6f62f6aa4598e2778
Harish <arishh2@gmail.com>
2020-09-25 23:30:11 +0530

    setup firebase livestream

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,14 +2,13 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyB1mDD2MLmghXbxZ4YSTK61KfYKrw2xAtQ',
-  authDomain: 'project-banana-4822.firebaseapp.com',
-  databaseURL: 'https://project-banana-4822.firebaseio.com',
-  projectId: 'project-banana-4822',
-  storageBucket: 'project-banana-4822.appspot.com',
-  messagingSenderId: '120654780626',
-  appId: '1:120654780626:web:bb5a034a16b3ad10da350b',
-  measurementId: 'G-92NW0Q5WD0',
+  apiKey: "AIzaSyB1mDD2MLmghXbxZ4YSTK61KfYKrw2xAtQ",
+  authDomain: "project-banana-4822.firebaseapp.com",
+  databaseURL: "https://project-banana-4822.firebaseio.com",
+  projectId: "project-banana-4822",
+  storageBucket: "project-banana-4822.appspot.com",
+  messagingSenderId: "120654780626",
+  appId: "1:120654780626:web:6bf2490f1eb56fc5da350b"
 };
 
 if (!firebase.apps.length) {
