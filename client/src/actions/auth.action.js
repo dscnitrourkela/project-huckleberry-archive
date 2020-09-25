18a64b7a1126f9fd025e33a412235b7b2365fdf9
Ritesh Patil <riteshsp2000@gmail.com>
2020-09-25 13:36:15 +0530

    Add login and badges actions

--- client/src/actions/auth.action.js
+++ client/src/actions/auth.action.js
@@ -1,3 +1,50 @@
+import axios from 'axios';
+
+import firebase from '../firebase';
 import AUTH from './types';
 
-export const login = () => (dispatch) => {};
+export const login = () => async (dispatch) => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  await firebase.auth().signInWithRedirect(provider);
+
+  try {
+    const result = await firebase.auth().getRedirectResult();
+    if (result.credential) {
+      const token = result.credential.accessToken;
+      const user = result.user;
+      localStorage.setItem('g_uuid', token);
+      dispatch({ type: AUTH.LOGIN, payload: user });
+    }
+  } catch (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+  }
+};
+
+export const setBadgesToken = (token) => async (dispatch) => {
+  try {
+    const {
+      data: { access_token },
+    } = await axios.post('https://badges.dscnitrourkela.tech/api/auth/convert-token', {
+      grant_type: 'convert_token',
+      client_id: 'BJlOifRQBb0zg0vVrbz0h62iaRhSrli8OJkt5Jz1',
+      backend: 'google-oauth2',
+      token: token,
+    });
+
+    if (access_token) {
+      const config = {
+        headers: { Authorization: `Bearer ${access_token}` },
+      };
+
+      const {
+        data: { uuid },
+      } = await axios.get('https://badges.dscnitrourkela.tech/api/sessions', config);
+      localStorage.setItem('uuid', uuid);
+      dispatch({ type: AUTH.BADGE_LOGIN, payload: true });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,4 +1,5 @@
 export const AUTH = {
   LOGIN: 'LOGIN',
   LOGOUT: 'LOGOUT',
+  BADGE_LOGIN: 'BADGE_LOGIN',
 };
